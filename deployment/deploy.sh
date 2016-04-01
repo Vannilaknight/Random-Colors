371857803a92a6be1824f74df7220938b03c79db
Taylor Boyd <taylorboyd@Taylors-MacBook-Pro.local>
2016-04-01 13:03:20 -0600

    wtf

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -13,15 +13,15 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 cd $DIR && cd ..
 
-scp -i $AUTH $DIR/setup.sh ubuntu@ec2-54-200-138-217.us-west-2.compute.amazonaws.com:~/
-
-ssh -i $AUTH ubuntu@ec2-54-200-138-217.us-west-2.compute.amazonaws.com "chmod +x setup.sh && sh setup.sh"
-
-tar -czvf project.tar.gz --exclude=node_modules/  --exclude=.git/ --exclude=.DS_Store --exclude=.idea/ .
-
-scp -i $AUTH project.tar.gz ubuntu@ec2-54-200-138-217.us-west-2.compute.amazonaws.com:~/deployment
-
-ssh -i $AUTH ubuntu@ec2-54-200-138-217.us-west-2.compute.amazonaws.com 'bash -s' < $DIR/runNode.sh
+#scp -i $AUTH $DIR/setup.sh ubuntu@ec2-54-200-138-217.us-west-2.compute.amazonaws.com:~/
+#
+#ssh -i $AUTH ubuntu@ec2-54-200-138-217.us-west-2.compute.amazonaws.com "chmod +x setup.sh && sh setup.sh"
+#
+#tar -czvf project.tar.gz --exclude=node_modules/  --exclude=.git/ --exclude=.DS_Store --exclude=.idea/ .
+#
+#scp -i $AUTH project.tar.gz ubuntu@ec2-54-200-138-217.us-west-2.compute.amazonaws.com:~/deployment
+#
+#ssh -i $AUTH ubuntu@ec2-54-200-138-217.us-west-2.compute.amazonaws.com 'bash -s' < $DIR/runNode.sh
 
 echo "Press any key to exit..."
 read -n 1 -s
