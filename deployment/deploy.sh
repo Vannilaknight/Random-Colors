71b6a66d0aa999be02aa52c64e6877a31f392055
Taylor Boyd <taylorboyd@Taylors-MacBook-Pro.local>
2016-04-01 13:16:58 -0600

    lets try this again

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -11,17 +11,12 @@ AUTH=~/.ssh/id_random-colors-box
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-cd $DIR && cd ..
+scp -i $AUTH $DIR/setup.sh ubuntu@ec2-54-200-138-217.us-west-2.compute.amazonaws.com:~/
 
-#scp -i $AUTH $DIR/setup.sh ubuntu@ec2-54-200-138-217.us-west-2.compute.amazonaws.com:~/
-#
-#ssh -i $AUTH ubuntu@ec2-54-200-138-217.us-west-2.compute.amazonaws.com "chmod +x setup.sh && sh setup.sh"
-#
-#tar -czvf project.tar.gz --exclude=node_modules/  --exclude=.git/ --exclude=.DS_Store --exclude=.idea/ .
-#
-#scp -i $AUTH project.tar.gz ubuntu@ec2-54-200-138-217.us-west-2.compute.amazonaws.com:~/deployment
-#
-#ssh -i $AUTH ubuntu@ec2-54-200-138-217.us-west-2.compute.amazonaws.com 'bash -s' < $DIR/runNode.sh
+ssh -i $AUTH ubuntu@ec2-54-200-138-217.us-west-2.compute.amazonaws.com "chmod +x setup.sh && sh setup.sh"
 
-echo "Press any key to exit..."
-read -n 1 -s
+tar -czvf project.tar.gz --exclude=node_modules/  --exclude=.git/ --exclude=.DS_Store --exclude=.idea/ .
+
+scp -i $AUTH project.tar.gz ubuntu@ec2-54-200-138-217.us-west-2.compute.amazonaws.com:~/deployment
+
+ssh -i $AUTH ubuntu@ec2-54-200-138-217.us-west-2.compute.amazonaws.com 'bash -s' < $DIR/runNode.sh
